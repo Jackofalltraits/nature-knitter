424883459b6cc15a038583b692599f823ab142db
jackkower <jackkower@gmail.com>
2020-10-20 22:09:03 -0700

    added build and nodemods

---
+++ build/precache-manifest.2151a74fbeb370cad23946ff607d25ad.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935898cfc7fab4fe1b6ab7d957e028e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf0ee23dc22b0b4ece0c",
+    "url": "/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "c69a1972f62b7f373d57",
+    "url": "/static/css/main.bb389691.chunk.css"
+  },
+  {
+    "revision": "cf0ee23dc22b0b4ece0c",
+    "url": "/static/js/2.6f435a76.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6f435a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69a1972f62b7f373d57",
+    "url": "/static/js/main.83b47834.chunk.js"
+  },
+  {
+    "revision": "120a9569e5e0b276d8ca",
+    "url": "/static/js/runtime-main.5e58e3cd.js"
+  },
+  {
+    "revision": "c56ad42f56fc8d1c1e70e5c4ac28e16e",
+    "url": "/static/media/backdrop1.c56ad42f.jpg"
+  },
+  {
+    "revision": "785c844696618ba7c98fca9aed1f7def",
+    "url": "/static/media/beanies.785c8446.jpg"
+  },
+  {
+    "revision": "c6f7ac1c4b8a85b0ca2267715ac243a7",
+    "url": "/static/media/blackscarf.c6f7ac1c.jpeg"
+  },
+  {
+    "revision": "7352363c5ce1e75e7c87d47245720066",
+    "url": "/static/media/blanket3.7352363c.png"
+  },
+  {
+    "revision": "d8d7c9296786d16e7229c807ac886ffd",
+    "url": "/static/media/blanket4.d8d7c929.jpg"
+  },
+  {
+    "revision": "a51e2767fe782ebcf42cc5594d196887",
+    "url": "/static/media/blanket5.a51e2767.jpg"
+  },
+  {
+    "revision": "5a9f5b8193d3a27c6dfe2e1594498c59",
+    "url": "/static/media/blankets.5a9f5b81.jpg"
+  },
+  {
+    "revision": "abc7eafd9c4c30cf0111af10f4343b20",
+    "url": "/static/media/bluescarf.abc7eafd.jpg"
+  },
+  {
+    "revision": "cbd660bb1b8b085284c5cd9be96098ce",
+    "url": "/static/media/brownscarf.cbd660bb.jpg"
+  },
+  {
+    "revision": "a08919d87c994efcfa675d89d6422e9d",
+    "url": "/static/media/coaster1.a08919d8.jpg"
+  },
+  {
+    "revision": "f22230e5a4249827d6f385eaaa9f02ed",
+    "url": "/static/media/coaster2.f22230e5.jpg"
+  },
+  {
+    "revision": "f5498b72a66135eb7e6b8cfc4ac28544",
+    "url": "/static/media/coaster3.f5498b72.jpg"
+  },
+  {
+    "revision": "2c6bfe4947f01c9362ffe403490b995b",
+    "url": "/static/media/coaster4.2c6bfe49.jpg"
+  },
+  {
+    "revision": "a007614b01213439254cb40607339297",
+    "url": "/static/media/coaster5.a007614b.jpg"
+  },
+  {
+    "revision": "c0ae770b168690f3bbb16a910b60a67a",
+    "url": "/static/media/coasters.c0ae770b.jpeg"
+  },
+  {
+    "revision": "9b192807a9a8a5d66c3f97c9226c2d3d",
+    "url": "/static/media/greyscarf.9b192807.jpg"
+  },
+  {
+    "revision": "f9e2d687a60e75bd19a2612fda4ce4d3",
+    "url": "/static/media/insta.f9e2d687.png"
+  },
+  {
+    "revision": "eaab15623400d611d7d7dd1c9caf6fae",
+    "url": "/static/media/jewel1.eaab1562.jpg"
+  },
+  {
+    "revision": "f95df6da6dd0f8025d0ef7821aa0e63a",
+    "url": "/static/media/jewel3.f95df6da.jpg"
+  },
+  {
+    "revision": "d175e81f1662fad044dcc03e340ed0ed",
+    "url": "/static/media/jewel4.d175e81f.jpg"
+  },
+  {
+    "revision": "46abd0d96a25bcaed9680c182d011087",
+    "url": "/static/media/jewelry.46abd0d9.jpg"
+  },
+  {
+    "revision": "8948215e0227f541c1996b4fdbef95cf",
+    "url": "/static/media/kandi1.8948215e.jpg"
+  },
+  {
+    "revision": "4757fdad9cef96afd3ae184e8656d2ba",
+    "url": "/static/media/kandi2.4757fdad.jpg"
+  },
+  {
+    "revision": "403ef8a53a71294e236b329fb9bfdd90",
+    "url": "/static/media/kandi3.403ef8a5.jpg"
+  },
+  {
+    "revision": "d34ed95ce0fff650793685e302fc5192",
+    "url": "/static/media/khandi.d34ed95c.jpg"
+  },
+  {
+    "revision": "05df0ff76275c3bd0fde66f1ea3a0e93",
+    "url": "/static/media/magnet.05df0ff7.jpg"
+  },
+  {
+    "revision": "e4eba5ab4a520e86837cfd72429ca4cc",
+    "url": "/static/media/magnet1.e4eba5ab.jpg"
+  },
+  {
+    "revision": "90e3960caf7bda6d0f9cfdd111d6ee11",
+    "url": "/static/media/magnet2.90e3960c.webp"
+  },
+  {
+    "revision": "ef3b75f50d8d13875d77a5f5740c6b72",
+    "url": "/static/media/magnet3.ef3b75f5.jpg"
+  },
+  {
+    "revision": "416eebdeb7af6a5288f03230e8ab6edf",
+    "url": "/static/media/magnet4.416eebde.jpg"
+  },
+  {
+    "revision": "d2ef73d93df92a6d36118fa895b241fd",
+    "url": "/static/media/nklogo.d2ef73d9.png"
+  },
+  {
+    "revision": "caf695716e3bc2f7c380ba1dc0bfd7a9",
+    "url": "/static/media/orangescarf.caf69571.jpg"
+  },
+  {
+    "revision": "563d5e0b6c5285462add23ba99d6b6f4",
+    "url": "/static/media/scarf1.563d5e0b.JPG"
+  },
+  {
+    "revision": "296072eab9a3b055dbce903f081cd17a",
+    "url": "/static/media/scarf2.296072ea.JPG"
+  },
+  {
+    "revision": "4b54bcc6136894bc4d7373074f0af987",
+    "url": "/static/media/scarf3.4b54bcc6.JPG"
+  },
+  {
+    "revision": "a64610c3f4b826d5153e23a861b44c14",
+    "url": "/static/media/scarves.a64610c3.jpg"
+  }
+]);
